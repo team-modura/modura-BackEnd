f1beb50f532082dc5ebd64fb47d10cd0bb59406e
조은정/이현 <165488194+E-hyeon@users.noreply.github.com>
2025-10-05 16:26:51 +0900

    chore : spring boot 초기세팅

---
+++ src/main/java/com/modura/modura_server/ModuraServerApplication.java
@@ -0,0 +1,13 @@
+package com.modura.modura_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuraServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuraServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modura/modura_server/ModuraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modura.modura_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuraServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
