a8f932359be9d8ad8d1dc47bdb59deb1cfad8695
Seoyeon <seoyeoneel02@gmail.com>
2025-11-04 22:22:48 +0900

    feat: 스프링 시큐리티 인터페이스 구현

--- src/main/java/com/modura/modura_server/domain/user/repository/UserRepository.java
+++ src/main/java/com/modura/modura_server/domain/user/repository/UserRepository.java
@@ -3,6 +3,11 @@ package com.modura.modura_server.domain.user.repository;
 import com.modura.modura_server.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findById(Long id);
+    Optional<User> findByOauthId(String oauthId);
+    Boolean existsByOauthId(String oauthId);
 }
