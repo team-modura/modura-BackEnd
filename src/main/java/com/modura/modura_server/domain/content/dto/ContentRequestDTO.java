d156c2fa8319768c2caf31549a2d7801ff478696
조은정/이현 <165488194+E-hyeon@users.noreply.github.com>
2025-11-10 03:40:41 +0900

    feat : 컨텐츠 리뷰 등록하기 api 구현

--- src/main/java/com/modura/modura_server/domain/content/dto/ContentRequestDTO.java
+++ src/main/java/com/modura/modura_server/domain/content/dto/ContentRequestDTO.java
@@ -1,5 +1,22 @@
 package com.modura.modura_server.domain.content.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Builder
 public class ContentRequestDTO {
 
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class ReviewReqDTO {
+        @Min(1) @Max(5)
+        private Integer rating;
+        @NotBlank
+        private String comment;
+    }
 }
--- src/main/java/com/modura/modura_server/domain/content/service/ContentCommandService.java
+++ src/main/java/com/modura/modura_server/domain/content/service/ContentCommandService.java
@@ -1,6 +1,10 @@
 package com.modura.modura_server.domain.content.service;
 
+import com.modura.modura_server.domain.content.dto.ContentRequestDTO;
+import com.modura.modura_server.domain.content.dto.ContentResponseDTO;
+
 public interface ContentCommandService {
     void like(Long contentId, Long userId);
     void unlike(Long contentId, Long userId);
+    void postContentReview(Long contentId, Long userId, ContentRequestDTO.ReviewReqDTO reviewReqDTO);
 }
