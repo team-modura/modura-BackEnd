645e5979bf6004dfc6be3a8d24581fd15407fe90
조은정/이현 <165488194+E-hyeon@users.noreply.github.com>
2025-11-11 20:04:37 +0900

    fix : 코드리뷰 반영

--- src/main/java/com/modura/modura_server/domain/user/service/UserCommandService.java
+++ src/main/java/com/modura/modura_server/domain/user/service/UserCommandService.java
@@ -3,5 +3,5 @@ package com.modura.modura_server.domain.user.service;
 import com.modura.modura_server.domain.user.dto.UserRequestDTO;
 
 public interface UserCommandService {
-    Void updateUser(Long userId, UserRequestDTO.UpdateUserDTO request);
+    void updateUser(Long userId, UserRequestDTO.UpdateUserDTO request);
 }
