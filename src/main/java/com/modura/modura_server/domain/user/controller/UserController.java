e8b5db174c551aa478362edeab3274ccb025f381
Seoyeon <seoyeoneel02@gmail.com>
2025-11-11 13:56:48 +0900

    refactor: 코드 리팩토링

--- src/main/java/com/modura/modura_server/domain/user/controller/UserController.java
+++ src/main/java/com/modura/modura_server/domain/user/controller/UserController.java
@@ -15,8 +15,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
